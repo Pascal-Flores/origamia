a0bc22f37f9c5db40ffba7fc96d6ccd2a98c9d17
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-01 15:58:48 +0200

    add Turtle solution for exercise 180

---
+++ src/exercises/pil-conception-algorithmes-sequence-repetition/cmp-repetition-n/180/solution.py
@@ -0,0 +1,21 @@
+# Solution de l'exercice 180 - La pièce LEGO octogonale
+# Le programme doit rester développé : aucune boucle dans la version montrée à l'élève.
+
+from turtle import *
+
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
+forward(55)
+right(45)
