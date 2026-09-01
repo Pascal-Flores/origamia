99ab88d2790cc83990b4172910e1582e89c18967
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-01 15:36:09 +0200

    align conception variants with historical exercise templates

---
+++ src/exercises/pil-conception-algorithmes-sequence-repetition/cmp-repetition-n/180/vittascience.py
@@ -0,0 +1,28 @@
+"""
+Auteur: Origamia
+Interface: python
+Nom du projet: 180 - La pièce LEGO octogonale
+Description: Observer un motif Turtle répété sans boucle.
+Toolbox: vittascience
+Mode: mixed
+
+Blocks: <xml xmlns="https://developers.google.com/blockly/xml"><block type="turtle_direction" id="ex180_f0"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex180_fd0"><field name="NUM">35</field></shadow></value><next><block type="turtle_turn" id="ex180_t0"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex180_ta0"><field name="NUM">45</field></shadow></value></block></next></block></xml>
+"""
+from turtle import *
+
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
+forward(35)
+right(45)
