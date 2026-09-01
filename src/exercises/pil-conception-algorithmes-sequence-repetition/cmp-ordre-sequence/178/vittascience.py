0af7df59baef1deaa0691659e37f75d7f01779b2
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-01 16:57:52 +0200

    update exercise 178 Vittascience flag project

--- src/exercises/pil-conception-algorithmes-sequence-repetition/cmp-ordre-sequence/178/vittascience.py
+++ src/exercises/pil-conception-algorithmes-sequence-repetition/cmp-ordre-sequence/178/vittascience.py
@@ -1,12 +1,12 @@
 """
 Auteur: Origamia
 Interface: python
-Nom du projet: 178 - Le pictogramme de la clé
-Description: Solution Turtle de l'exercice 178 : un anneau rond, une tige et un panneton en angle droit.
+Nom du projet: 178 - Le drapeau de départ
+Description: Solution Turtle de l'exercice 178 : un mât vertical et un drapeau rectangulaire.
 Toolbox: vittascience
 Mode: mixed
 
-Blocks: <xml xmlns="https://developers.google.com/blockly/xml"><block type="turtle_circle" id="ex178_circle" x="30" y="30"><value name="RADIUS"><shadow type="math_number" id="ex178_radius"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn1"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle1"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward1"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist1"><field name="NUM">50</field></shadow></value><next><block type="turtle_direction" id="ex178_forward2"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist2"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn2"><field name="DIR">LEFT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle2"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward3"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist3"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn3"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle3"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward4"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist4"><field name="NUM">50</field></shadow></value></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></xml>
+Blocks: <xml xmlns="https://developers.google.com/blockly/xml"><block type="turtle_turn" id="ex178_turn1" x="30" y="30"><field name="DIR">LEFT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle1"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward1"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist1"><field name="NUM">50</field></shadow></value><next><block type="turtle_direction" id="ex178_forward2"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist2"><field name="NUM">50</field></shadow></value><next><block type="turtle_direction" id="ex178_forward3"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist3"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn2"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle2"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward4"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist4"><field name="NUM">50</field></shadow></value><next><block type="turtle_direction" id="ex178_forward5"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist5"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn3"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle3"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward6"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist6"><field name="NUM">50</field></shadow></value><next><block type="turtle_turn" id="ex178_turn4"><field name="DIR">RIGHT</field><value name="DISTANCE"><shadow type="math_number" id="ex178_angle4"><field name="NUM">90</field></shadow></value><next><block type="turtle_direction" id="ex178_forward7"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist7"><field name="NUM">50</field></shadow></value><next><block type="turtle_direction" id="ex178_forward8"><field name="DIR">FORWARD</field><value name="DISTANCE"><shadow type="math_number" id="ex178_dist8"><field name="NUM">50</field></shadow></value></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></xml>
 
 Projet généré pour import dans Vittascience.
 Ce fichier contient le code textuel ainsi que le code blocs. Il peut être importé de nouveau
@@ -16,11 +16,15 @@ sur l'interface http://vittascience.com/python
 
 import turtle
 
-turtle.circle(50)
-turtle.right(90)
-turtle.forward(50)
-turtle.forward(50)
 turtle.left(90)
 turtle.forward(50)
+turtle.forward(50)
+turtle.forward(50)
 turtle.right(90)
+turtle.forward(50)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
 turtle.forward(50)
