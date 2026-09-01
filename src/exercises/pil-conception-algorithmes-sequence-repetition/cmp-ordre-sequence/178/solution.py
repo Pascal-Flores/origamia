f07efa07ef9f35792ded365a671efc7b5fc7c096
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-01 15:56:56 +0200

    add turtle solution for exercise 178

---
+++ src/exercises/pil-conception-algorithmes-sequence-repetition/cmp-ordre-sequence/178/solution.py
@@ -0,0 +1,14 @@
+# Solution de l'exercice 178 - Fanion de départ
+# Les 8 blocs sont déjà paramétrés dans l'exercice Vittascience.
+# L'élève doit uniquement les assembler dans cet ordre.
+
+from turtle import *
+
+left(90)
+forward(120)
+right(90)
+forward(80)
+right(120)
+forward(80)
+right(120)
+forward(80)
