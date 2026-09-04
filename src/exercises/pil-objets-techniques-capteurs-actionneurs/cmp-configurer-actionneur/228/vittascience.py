5906edf94f1a78124a9a518a4822896a0bc87be3
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-04 18:11:52 +0200

    Ajouter le projet Vittascience de l'exercice 228

---
+++ src/exercises/pil-objets-techniques-capteurs-actionneurs/cmp-configurer-actionneur/228/vittascience.py
@@ -0,0 +1,16 @@
+"""
+Auteur: Origamia
+Interface: microbit
+Nom du projet: 228 - Signal au point de rendez-vous
+Description: Corriger la fréquence et la durée du signal sonore de la balise.
+Toolbox: vittascience
+Mode: mixed
+
+Blocks: <xml xmlns="https://developers.google.com/blockly/xml"><block type="on_start" id="ex228_start_0" deletable="false" x="20" y="20"><statement name="DO"><block type="actuators_music_playFrequency" id="ex228_sound_0"><field name="PIN">pin_speaker</field><value name="FREQUENCY"><shadow type="math_number" id="ex228_frequency_0"><field name="NUM">440</field></shadow></value><value name="DURATION"><shadow type="math_number" id="ex228_duration_0"><field name="NUM">500</field></shadow></value></block></statement></block></xml>
+"""
+from microbit import *
+import music
+
+
+# Reglages provisoires a corriger par l'eleve dans l'interface.
+music.pitch(440, duration=500)
