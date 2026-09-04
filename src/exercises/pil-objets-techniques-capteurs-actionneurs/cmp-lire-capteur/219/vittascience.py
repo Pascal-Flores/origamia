ddef48ea399a80ce3f403c8c014f6f422d811510
Pascal Flores <72205230+Pascal-Flores@users.noreply.github.com>
2026-09-04 17:39:19 +0200

    Ajouter le projet Vittascience importable de 219

---
+++ src/exercises/pil-objets-techniques-capteurs-actionneurs/cmp-lire-capteur/219/vittascience.py
@@ -0,0 +1,22 @@
+"""
+Auteur: Origamia
+Interface: microbit
+Nom du projet: 219 - Assez sombre pour projeter ?
+Description: Choisir le capteur qui mesure la luminosité de la salle.
+Toolbox: vittascience
+Mode: mixed
+
+Blocks: <xml xmlns="https://developers.google.com/blockly/xml"><block type="on_start" id="ex219_start_0" deletable="false" x="20" y="20" /><block type="forever" id="ex219_forever_0" x="350" y="20"><statement name="DO"><block type="communication_serialWrite" id="ex219_display_0"><mutation newlines="false" /><next><block type="io_pause" id="ex219_pause_0"><field name="UNIT">SEC</field><value name="TIME"><shadow type="math_number" id="ex219_pause_1"><field name="NUM">1</field></shadow></value></block></next></block></statement></block><block type="sensors_getLight" id="ex219_candidate1_0" x="850" y="20" /><block type="io_micro_getSoundLevel" id="ex219_candidate2_0" x="850" y="130" /></xml>
+"""
+# Le programme est genere a partir des blocs du projet Vittascience.
+# Le programme est volontairement incomplet : un bloc doit etre place dans print().
+from microbit import *
+import utime
+
+while True:
+    print()
+    utime.sleep(1)
+
+# Blocs candidats, laisses non connectes dans l'espace de travail.
+display.read_light_level()
+microphone.sound_level()
